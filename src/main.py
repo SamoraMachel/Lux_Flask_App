c3a66d291fd07b13ae484ad6e1f1cbcd0fb3b6b5
Samora Machel <samora@me.laptop>
2021-09-15 07:46:56 +0300

    Added templates and statics created the authentication page

--- src/main.py
+++ src/main.py
@@ -1,4 +1,16 @@
-from flask import Flask
+from flask import Flask, request
+from flask.templating import render_template
 
 app = Flask(__name__)
 
+@app.route('/', methods=['POST', 'GET'])
+def login_user():
+    if request.method == 'POST':
+        return "Posted Data"
+    return render_template('login.html')
+    
+    
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
